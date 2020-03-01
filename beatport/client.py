4b677612f761377165e0488737c11d4a479d5f00
oier26 <oier_26@hotmail.com>
2020-03-01 14:49:49 +0100

    Add comment header in all files

--- beatport/client.py
+++ beatport/client.py
@@ -13,6 +13,10 @@
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
 
+"""
+Python Beatport API <https://oauth-api.beatport.com> Wrapper.
+"""
+
 import logging
 from urllib.parse import urlencode
 
