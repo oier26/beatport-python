2cf20f7126da1c0334b867f1c03d9c3b70e615bb
oier26 <oier_26@hotmail.com>
2020-03-02 17:45:54 +0100

    Add new test cases

--- tests/test_client.py
+++ tests/test_client.py
@@ -16,10 +16,26 @@
 """
 Python Beatport API <https://oauth-api.beatport.com> Wrapper.
 """
+import pytest
 
 import beatport
 
 
-def test_authorize():
-    client = beatport.Client()
-    assert True
+@pytest.mark.parametrize("api_key", ['57713c3906af6f5def151b33601389176b37b429'])
+@pytest.mark.parametrize("api_secret", ['b3fe08c93c80aefd749fe871a16cd2bb32e2b954'])
+@pytest.mark.parametrize("access_token", ['77beecc7fd51e69142d5d78312421f8ef421981b'])
+@pytest.mark.parametrize("access_secret", ['7b6a2178d4315ea2bbf47597306f445296465839'])
+@pytest.mark.parametrize("auth_response", ['oauth_token=ebef014e0fac2a767c4b579840e24da887273369&oauth_verifier=' +
+                                           '01749676015e0349f0aaf1fcccf97b9c&oauth_callback_confirmed=true'])
+@pytest.fixture
+def fixture_client(api_key, api_secret, access_token, access_secret):
+    client = beatport.Client(api_key=api_key, api_secret=api_secret,
+                             access_token=access_token, access_secret=access_secret)
+    return client
+
+
+@pytest.mark.parametrize("url", ['/catalog/3/tracks'])
+@pytest.mark.parametrize("track_id", ['12857425'])
+def test_authorize(fixture_client, url, track_id):
+    assert fixture_client.get_query(url, id=track_id) is not None
+    print(fixture_client.get_query(url, id=track_id))
--- tests/test_refresh_metadata.py
+++ tests/test_refresh_metadata.py
@@ -16,3 +16,63 @@
 """
 Python Beatport API <https://oauth-api.beatport.com> Wrapper.
 """
+import os
+from pathlib import Path
+
+import pytest
+from mediafile import MediaFile, FileTypeError
+
+import beatport
+
+api_key = '57713c3906af6f5def151b33601389176b37b429'
+api_secret = 'b3fe08c93c80aefd749fe871a16cd2bb32e2b954'
+access_token = '77beecc7fd51e69142d5d78312421f8ef421981b'
+access_secret = '7b6a2178d4315ea2bbf47597306f445296465839'
+auth_response = 'oauth_token=ebef014e0fac2a767c4b579840e24da887273369&oauth_verifier=' \
+                '01749676015e0349f0aaf1fcccf97b9c&oauth_callback_confirmed=true'
+
+
+def get_client():
+    client = beatport.Client(api_key=api_key, api_secret=api_secret,
+                             access_token=access_token, access_secret=access_secret)
+    return client
+
+
+def find_audio_files_in_folder(folder):
+    audio_files_list = []
+    for root, dirs, files in os.walk(folder):
+        audio_files_list.extend(
+            [f'{root}/{file}' for file in files if Path(file).suffix in ['.mp3', '.m4a', '.flac', '.wav']]
+        )
+    return audio_files_list
+
+
+def get_media_file(audio_file_path):
+    try:
+        media_file = MediaFile(audio_file_path)
+        return media_file
+    except FileTypeError:
+        pytest.fail(f"Audio file {audio_file_path} was not correctly opened.")
+
+
+@pytest.mark.parametrize("folder", ['C:/Users/oarroniz/Downloads/Music',
+                                    'C:/Users/oarroniz/Music/iTunes/iTunes Media/Music'])
+def test_full_tagging_process(folder):
+    client = get_client()
+    for audio_file_path in find_audio_files_in_folder(folder):
+        media_file = get_media_file(audio_file_path)
+        result = client.get_query(
+            '/catalog/3/search/',
+            query=media_file.title,
+            facets=f"{'artistName:' + media_file.artist.split(',')[0] or None}"
+        )
+        object_track = next((objectTrack for objectTrack in result if objectTrack.get("type") == "track"), None)
+
+        if object_track is not None:
+            genre_list = [genre.get('name') for genre in object_track.get('genres')] + \
+                         [subgenre.get('name') for subgenre in object_track.get('subGenres')]
+            beatport_title = object_track.get("title")
+            beatport_artist = ", ".join([artist.get("name") for artist in object_track.get("artists")])
+            print(f'{media_file.artist}|{media_file.title}|{", ".join(genre_list)}|{beatport_artist}|{beatport_title}')
+        else:
+            print(f'{media_file.artist}|{media_file.title}|None|None')
